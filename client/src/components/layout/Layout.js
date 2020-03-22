2651387a53beec967dc6617e0c6c3f9e1b8565a8
Roopesh Patel <roops.pesh@gmail.com>
2020-03-22 17:49:31 +0000

    update layout 2

---
+++ client/src/components/layout/Layout.js
@@ -0,0 +1,174 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import HomeIcon from "@material-ui/icons/Home";
+import SentimentVerySatisfiedIcon from "@material-ui/icons/SentimentVerySatisfied";
+import { NavLink } from "react-router-dom";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+const drawerWidth = 200;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}));
+
+function ResponsiveDrawer(props) {
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const breakpointMatches = useMediaQuery(theme.breakpoints.down("xs"));
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [selectedLink, setSelectedLink] = React.useState(0);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const handleListItemClick = (event, index) => {
+    setSelectedLink(index);
+    if (breakpointMatches) {
+      handleDrawerToggle();
+    }
+  };
+
+  const drawerLinks = [
+    {
+      icon: <HomeIcon />,
+      primary: "Home",
+      to: "/"
+    },
+    {
+      icon: <HomeIcon />,
+      primary: "About",
+      to: "/about"
+    }
+  ];
+
+  const drawer = drawerLinks => {
+    const drawerlinksRendered = drawerLinks.map((link, index) => {
+      return (
+        <li>
+          <ListItem
+            component={NavLink}
+            to={link.to}
+            button
+            key={link.primary}
+            selected={selectedLink === index}
+            onClick={event => handleListItemClick(event, index)}
+          >
+            <ListItemIcon>
+              {link.icon ? <ListItemIcon>{link.icon}</ListItemIcon> : null}
+            </ListItemIcon>
+            <ListItemText primary={link.primary} />
+          </ListItem>
+        </li>
+      );
+    });
+    return (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>{drawerlinksRendered}</List>
+      </div>
+    );
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" noWrap>
+            {props.appName}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="Navigation">
+        <Hidden smUp>
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer(drawerLinks)}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown>
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            <div className={classes.toolbar}>{drawer(drawerLinks)}</div>
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {props.children}
+      </main>
+    </div>
+  );
+}
+
+export default ResponsiveDrawer;
